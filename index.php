d2ba0804a3c8c8aae2270bd64b06efa44cb6ca1e
Felipe <felipeagostinis7@gmail.com>
2026-05-12 20:01:22 -0300

    estilizando o meu header

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Be fit</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
     <header>
@@ -11,7 +14,7 @@
 
             <div class="logo">
                     <a href="index.php" title="Home" alt="Home">
-                        <img src="img/logobefitescura" title="Logo" alt="Logo">
+                        <img src="img/logobefitescura.jpeg" title="Logo" alt="Logo">
                     </a>
             </div>
 
@@ -32,11 +35,23 @@
         </div>
 
         <nav>
-            <a href="#">Inicio</a>
-            <a href="#">Produtos</a>
-            <a href="#">Novidades</a>
-            <a href="#">Contato</a>
-            <a href="#">Quem Somos</a>
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Produtos</a>
+                </li>
+                <li>
+                    <a href="#">Novidades</a>
+                </li>
+                <li>
+                    <a href="#">Contato</a>
+                </li>
+                <li>
+                    <a href="#">Quem Somos</a>
+                </li>
+            </ul>
         </nav>
 
     </header>
