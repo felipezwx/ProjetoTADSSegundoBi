0b37e5f18ebf828ab3b799376cce81e894ad7135
Felipe <felipeagostinis7@gmail.com>
2026-05-12 12:02:37 -0300

    mudando o index para php inves de html, e introduzindo o header do site

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Be fit</title>
+</head>
+<body>
+    <header>
+        <div class="topo-header">
+
+            <div class="logo">
+                    <a href="index.php" title="Home" alt="Home">
+                        <img src="img/logobefitescura" title="Logo" alt="Logo">
+                    </a>
+            </div>
+
+            <div class="busca">
+                <input type="text" placeholder="Buscar">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>      
+            
+            <div class="icones">
+                <div class="icon-user">
+                    <i class="fa-regular fa-user"></i>
+                </div>
+
+                <div class="icon-carrinho">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>
+            </div>
+        </div>
+
+        <nav>
+            <a href="#">Inicio</a>
+            <a href="#">Produtos</a>
+            <a href="#">Novidades</a>
+            <a href="#">Contato</a>
+            <a href="#">Quem Somos</a>
+        </nav>
+
+    </header>
+</body>
+</html>
