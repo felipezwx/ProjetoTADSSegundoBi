ae46c7c8dacc9f48ae4bec1ce7424ef4cb5343dc
Felipe <felipeagostinis7@gmail.com>
2026-05-22 02:58:40 -0300

    adicionando as primeiras raquetes, e ja estilizando elas

--- index.php
+++ index.php
@@ -78,6 +78,65 @@
 
     </section>
 
+    <section class="raquetes">
+
+        <div class="raquetes-grid">
+
+            <div class="raquetes-blocos">
+                <div class="raquetes-imagens">
+                    <img src="img/raquetemormaivt.webp" alt="Raquete de Beach Tennis Mormaii Vitoria">
+                </div>
+                <h3 class="raquete-nome">Raquete de Beach Tennis Mormaii Vitoria Marquezini</h3>
+
+                <div class="raquetes-preco">
+                    <strong>R$ 2.499,90</strong>
+                </div>
+
+                <button class="botao-comprar">Comprar</button>
+            </div>
+
+            <div class="raquetes-blocos">
+                <div class="raquetes-imagens">
+                    <img src="img/raqueteheroesstarlight.webp" alt="Raquete de Beach Tennis Starlight">
+                </div>
+                <h3 class="raquete-nome">Raquete de Beach Tennis Heroes Starlight Ruby 2026</h3>
+
+                <div class="raquetes-preco">
+                    <strong>R$ 3.499,00</strong>
+                </div>
+
+                <button class="botao-comprar">Comprar</button>
+            </div>
+
+            <div class="raquetes-blocos">
+                <div class="raquetes-imagens">
+                    <img src="img/raquetekonagladiator.png" alt="Raquete de Beach Tennis Kona Gladiator">
+                </div>
+                <h3 class="raquete-nome">Raquete de Beach Tennis Kona Gladiator Steel 2026</h3>
+
+                <div class="raquetes-preco">
+                    <strong>R$ 2.499,00</strong>
+                </div>
+
+                <button class="botao-comprar">Comprar</button>
+            </div>
+
+            <div class="raquetes-blocos">
+                <div class="raquetes-imagens">
+                    <img src="img/raquetefobelhusky.png" alt="Raquete de Beach Tennis Fobel Husky">
+                </div>
+                <h3 class="raquete-nome">Raquete de Beach Tennis Fobel Husky 25/26</h3>
+
+                <div class="raquetes-preco">
+                    <strong>R$ 2.159,90</strong>
+                </div>
+
+                <button class="botao-comprar">Comprar</button>
+            </div>
+        </div>
+
+    </section>
+
     <script>
 
         let slide = 0;
