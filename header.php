fdf97c8e3efd52610bfef1fdce5cd703aba7847d
Felipe <felipeagostinis7@gmail.com>
2026-05-24 20:57:10 -0300

    criando header novo, utilizando templete em PHP

---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Be fit</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+    <header>
+        <div class="topo-header">
+
+            <div class="logo">
+                    <a href="index.php" title="Home" alt="Home">
+                        <img src="img/logobefitclara.jpeg" title="Logo" alt="Logo">
+                    </a>
+            </div>
+
+            <div class="busca">
+                <input type="text" placeholder="Buscar">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>      
+            
+            <div class="icones">
+                <div class="icon-user">
+                    <i class="fa-regular fa-user"></i>
+                </div>
+
+                <div class="icon-carrinho">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>
+            </div>
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Produtos</a>
+                </li>
+                <li>
+                    <a href="#">Novidades</a>
+                </li>
+                <li>
+                    <a href="#">Contato</a>
+                </li>
+                <li>
+                    <a href="#">Quem Somos</a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+</body>
+</html>
