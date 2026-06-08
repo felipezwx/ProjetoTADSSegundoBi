fea956bae68540f09d3d6cea80b08c4ade72e580
Felipe Agostinis <felipeagostinis7@gmail.com>
2026-06-07 21:57:37 -0300

    primeira parte de descrição de produto

--- infoprodutos/raquetevitoriamormaii.php
+++ infoprodutos/raquetevitoriamormaii.php
@@ -14,9 +14,32 @@
     <section class="produto">
 
         <div class="produto-imagem">
-            <img src="img:"
+            <img src="/ProjetoTADSSegundoBi/img/raquetemormaivt.webp">
         </div>
 
+        <div class="produto-info">
+
+            <h1>Raquete De Beach Tennis Mormaii Vitoria Marchezini</h1>
+         
+            <p>
+                A Raquete de Beach Tennis Mormaii Vitória Marchezini foi desenvolvida para oferecer o equilíbrio ideal entre potência, controle e conforto em quadra. Com design aprimorado em relação à versão anterior, proporciona ótima absorção de impacto e excelente desempenho nas jogadas.
+            </p>
+
+            <h2>R$ 2.499,00</h2>
+
+            <a href="https://wa.me/5544998501379?text=Olá,%20tenho%20interesse%20na%20Raquete%20Mormaii"
+            class="btn-whatsapp">
+                Comprar pelo WhatsApp
+            </a>
+
+            <br><br>
+
+            <a href="/ProjetoTADSSegundoBi/index.php" class="btn-voltar">
+                Voltar Inicio
+            </a>
+        </div>
+
+        
     </section>
 </body>
 </html>
